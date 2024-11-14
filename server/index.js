7abe8182eb5e36e6485dcc0971b0b0de37e965c7
raseela786 <raseelakk786@gmail.com>
2024-11-14 12:02:06 +0530

    foods web

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://food-app-wine-ten.vercel.app"],
     credentials:true,
 }))
 connectDB();
