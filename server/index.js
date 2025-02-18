afb2176668b87bfc1efe6f43afb405f3bc2e0abd
raseela786 <raseelakk786@gmail.com>
2025-02-18 11:59:29 +0530

    first commit raaz

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:["http://localhost:5173","https://foodweborder.vercel.app"],
+    origin:["http://localhost:5173","https://foodwebreraz-client.vercel.app"],
     credentials: true,
     methods :['POST','GET',"PUT","DELETE","OPTION"],
  
